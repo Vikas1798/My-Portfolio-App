97a56adc1659c54da11bd33742afacf353df45e2
Vikas1798 <hkvikas1798@gmail.com>
2024-09-08 15:26:43 +0530

    ui updated

--- src/App.jsx
+++ src/App.jsx
@@ -58,11 +58,11 @@ const App = () => {
                 <p className="text-md text-fifth pb-10 flex items-center justify-center">© 2021 - {new Date().getFullYear()}. Vikas H K All rights reserved.</p>
                 {/* <p className="text-sm text-fifth mt-2 pb-16 xl:pb-10 flex items-center justify-center text-center">About this website : built with , Vite, Tailwind CSS, Framer Motion and Vercel hosting.</p> */}
 
-                <div className="flex xl:hidden h-[50px] fixed bottom-2   w-full z-50 p-2">
-                    <div className="w-[90%] sm:w-[70%] grid grid-cols-4 gap-2 items-center mx-auto bg-[#2b2b2cbf] rounded-2xl bg-opacity-60 backdrop-blur-sm">
+                <div className="flex xl:hidden h-[60px] fixed bottom-2   w-full z-50 p-2">
+                    <div className="w-[90%] sm:w-[70%] grid grid-cols-4 gap-2 items-center mx-auto bg-[#2b2b2cbf] rounded-full bg-opacity-60 backdrop-blur-sm">
                         {
                             allSegment?.map((d, i) => (
-                                <p onClick={() => updateTab(d?.slug)} className={`text-xs sm:text-sm flex items-center justify-center  ${state.segment === d.slug ? ' text-basicGreen' : ' text-seventh'}`} key={i}>{d?.name}</p>
+                                <p onClick={() => updateTab(d?.slug)} className={` text-xs sm:text-sm flex items-center justify-center  ${state.segment === d.slug ? ' text-basicGreen' : ' text-seventh'}`} key={i}>{d?.name}</p>
                             ))
                         }
                     </div>
@@ -73,3 +73,4 @@ const App = () => {
 }
 
 export default App;
+//
