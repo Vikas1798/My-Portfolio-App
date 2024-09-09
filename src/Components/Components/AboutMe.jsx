a37ebdc5eda53adf57bd83ff20e43cb23055698e
Vikas1798 <hkvikas1798@gmail.com>
2024-09-09 12:34:43 +0530

    required details updated

--- src/Components/Components/AboutMe.jsx
+++ src/Components/Components/AboutMe.jsx
@@ -16,7 +16,7 @@ const AboutMe = () => {
                 I'm a Frontend Engineer from Bangalore, India, specializing in creating dynamic and responsive web applications. I thrive on transforming complex challenges into sleek, efficient, and user-friendly solutions.
             </p>
             <p className='text-sm  text-fifth mt-5 leading-6'>
-                My focus is on building applications that are not only functional and intuitive but also visually appealing and engaging. I bring a personal touch to every project, ensuring your web app is both high-performing and uniquely tailored to your needs. My goal is to deliver your brand’s message through seamless and innovative web experiences. I have developed web solutions for renowned brands, enhancing their online presence with cutting-edge technologies.
+                My focus is on building applications that are not only functional and intuitive but also visually appealing and engaging. I bring a personal touch to every project, ensuring your web app is both high-performing and uniquely tailored to your needs. My goal is to deliver your brand’s message through seamless and innovative web experiences.
             </p>
             <div className='mt-5'>
                 <div className='flex items-center gap-2'>
